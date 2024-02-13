3cc38069c1be5e5f67f710c6fd5d2fe7d45ab527
sumitkk10 <sumitkk01010@gmail.com>
2024-02-13 17:30:49 +0530

    checkpoint in (b)

--- code/clustering.py
+++ code/clustering.py
@@ -55,7 +55,49 @@ def louvain_community_detection(G, t, sorted_years, year_nodes):
     # returns a list of tuples [{}, {}]
     return communities
     
+T = int(input("Enter the year till which you want the clustering of papers to be done: "))
+communities = louvain_community_detection(G, T, sorted_years, year_nodes)
+print(communities)
 
-communities = louvain_community_detection(G, 1998, sorted_years, year_nodes)
+# Lets plot the number of clusters over time
+here = []
+num_of_communities = []
+for cur_year in sorted_years:
+  for node in year_nodes[cur_year]:
+    if node not in here:
+      here.append(node)
+  cur_graph = G.subgraph(here)
+  communities = nx.community.louvain_communities(cur_graph)
+  num_of_communities.append(len(communities))
+
+plt.plot(sorted_years, num_of_communities, marker='o')
+plt.xlabel('Year')
+plt.ylabel('Number of Communities')
+plt.title('Number of Communities in Graph Over Time')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+# Lets plot the size of the largest cluster over time
+here = []
+largest_communities = []
+for cur_year in sorted_years:
+  for node in year_nodes[cur_year]:
+    if node not in here:
+      here.append(node)
+  cur_graph = G.subgraph(here)
+  communities = nx.community.louvain_communities(cur_graph)
+  mx = 0
+  for community in communities:
+    mx = max(mx, len(community))
+  largest_communities.append(mx)
+
+plt.plot(sorted_years, largest_communities, marker='o')
+plt.xlabel('Year')
+plt.ylabel('Size of largest community')
+plt.title('Size of largest community in Graph Over Time')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
 
 
